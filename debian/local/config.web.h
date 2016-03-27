2ea1625db4f66b5c01a1fa291953796aa63213de
Hugo Lefeuvre <hle@debian.org>
2016-03-27 17:49:23 +0200

    New upstream release.
    * New upstream release.
    * debian/control:
      - Bump Standards-Version to 3.9.7.
      - Add a dependency on libxft-dev and libfreetype6-dev to match
        new release requirements.
      - Replace uxterm by stterm in the Recommends field to match new release
        recommendations.
      - Fix broken Vcs-Browser field.
      - Update Vcs-Git field to use an encrypted transport protocol.
    * debian/copyright:
      - Update copyright years.
    * debian/watch:
      - Change link to point to the official git repository (releases
        are more up-to-date there).
    * debian/patches/*:
      - Refresh patches for new upstream release.
    * debian/local/*:
      - Refresh config files for new upstream release.
    * debian/rules:
      - Enable all hardening options to compile as a PIE and add the bindnow
        flag.

--- debian/local/config.web.h
+++ debian/local/config.web.h
@@ -1,5 +1,8 @@
+/* See LICENSE file for copyright and license details. */
+
 /* appearance */
-static const char font[]            = "fixed";
+static const char *fonts[] = { "fixed" };
+static const char dmenufont[]            = "fixed";
 static const char normbordercolor[] = "#cccccc";
 static const char normbgcolor[]     = "#cccccc";
 static const char normfgcolor[]     = "#000000";
@@ -8,35 +11,43 @@ static const char selbgcolor[]      = "#0066ff";
 static const char selfgcolor[]      = "#000000";
 static unsigned int borderpx        = 0;
 static unsigned int snap            = 32;
-static Bool showbar                 = False;
-static Bool topbar                  = False;
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 
 /* tagging */
 static const char *tags[] = { "web" };
 
-static Rule rules[] = {
-	{0}
+static const Rule rules[] = {
+    {0}
 };
 
 /* layout(s) */
-static float mfact      = 0.55;
-static const int nmaster      = 1;    /* number of clients in master area */
-static Bool resizehints = False; /* False means respect size hints in tiled resizals */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
-Layout layouts[] = {
-	/* symbol		function */
-	{ "[M]",		monocle },
+static const Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "[M]",      monocle },
 };
 
 /* key definitions */
 #define MODKEY Mod1Mask
-Key keys[] = { \
-	/* modifier			key		function	argument */ \
-	{ MODKEY|ShiftMask, XK_c,   killclient,     {0} },
-	{ MODKEY,		    XK_q,   killclient,		{0} }, \
-	{ MODKEY,		    XK_F4,  killclient,		{0} }, \
+
+/* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+//static const char *termcmd[]  = { "x-terminal-emulator", NULL };
+
+static Key keys[] = {
+    /*  modifier        key     function        argument */ \
+    { MODKEY|ShiftMask, XK_c,   killclient,     {0} },
+    { MODKEY,           XK_q,   killclient,     {0} }, \
+    { MODKEY,           XK_F4,  killclient,     {0} }, \
 };
 
+/* button definitions */
+/* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-	{0}
+    { 0 }
 };
